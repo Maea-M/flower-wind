d5cae6b3e5cd48ff04566d87881d5c258b251691
Morgane Mahé <maeamahe@gmail.com>
2024-03-23 16:39:33 +0100

    add reset.css style.css

--- modules/Search.js
+++ modules/Search.js
@@ -1,22 +1,74 @@
-/* Barre de recherche
-- récupérer la ville saisie par l'utilisateur
-- à partir du nom, trouver la lattitude et la longitude correspondante
-Bonus : si la ville saisie n'est pas connue, elle va indiquer une erreur au visiteur
-Bonus :  on pourrait proposer des suggestions de villes au moment où l'utilisateur tape
-Bonus: enregistrer les dernières recherches
+/* Ma bare de recherche : 
+- [x] : Récupérer la ville renseignée par mon visiteur
+- [ ] : À partir du nom va aller trouver la latitude 
+et la longitude correspondantes
+- BONUS : Si la ville renseignée n'est pas dans la base de données
+(json) elle doit indiquer l'erreur au visiteur
+- BONUS : Autocompletion dans les noms de villes
+- BONUS : Enregistrer les dernières recherches 
 */
 
 class Search {
 
     constructor() {
-        /* Définir les propriétés puis les méthodes*/
-        this.input = 'my nput'
+        /* 1er temps = il sert à définir les variables, les propriétés */
+        this.input = document.querySelector('.js-search-input')
+        this.form = document.querySelector('.js-search-form')
+        this.cities = []
+        /* 2nd temps = lancer les fonctions, les méthodes */
         this.init()
     }
-        init() {
-            /* méthode pour lancer toutes les fonctions de ma classe*/
-            console.log('je suis dans init')
-        }    
 
+    init() {
+        /* Méthode pour lancer toutes les fonction de ma classe */
+        this.getCities()
+        this.watchUserInput()
+    }
+
+    watchUserInput() {
+        this.form.addEventListener('submit', (event) => {
+            event.preventDefault()
+            this.getLatLong()
+        })
+    } 
+
+    getLatLong() {
+        const name = this.input.value
+        const cityData = this.getCityData(name)
+        if (cityData) {
+            const lat = cityData.lat
+            const long = cityData.lng
+        } else {
+            alert('La ville renseignée n\'exsite pas')
+        }
+    }
+
+    getCities() {
+        fetch('../data/france-cities.json')
+            .then(response => response.json())
+            .then(data => {
+                this.cities = data
+            })
+    }
+
+    getCityDataWithFor(cityName) {
+        const cityNameLower = cityName.toLowerCase()
+        let cityData = {}
+        for (let i = 0; i < this.cities.length; i++) {
+            const cityNameInDataLower = this.cities[i].city.toLowerCase()
+            if (cityNameInDataLower === cityNameLower) {
+                cityData = this.cities[i]
+                break
+            }
+        }
+        return cityData
+    }
+
+    getCityData(userCityName) {
+        const userCityNameLower = userCityName.toLowerCase()
+        const data = this.cities.find( cityObject => cityObject.city.toLowerCase() === userCityNameLower)
+        return data
+    }
 }
+
 export { Search }
